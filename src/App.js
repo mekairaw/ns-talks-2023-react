89ad19a64c467bda67f7ee949cf2d60483c96905
Dustin Espinoza <dustinespinoza@Admins-MacBook-Pro-5.local>
2023-07-14 18:40:58 -0600

    Created react app fully functional with Stencil

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import 'stencil-accordion-mekairaw';
 
 function App() {
+  const array = [
+    {
+      label: 'Accordion 1',
+      description: 'Lorem ipsum',
+      color: '#439ECA',
+      width: '60%'
+    },
+    {
+      label: 'Accordion 2',
+      description: 'Lorem ipsum',
+      color: '#7EC74A',
+      width: '300px'
+    },
+    {
+      label: 'Accordion 3',
+      description: 'Lorem ipsum',
+      color: '#F8CD41',
+      width: '700px'
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        array.map((array) => {
+          return (
+            <my-accordion 
+            label={array.label}
+            description={array.description}
+            color={array.color}
+            width={array.width}>
+            </my-accordion>
+          )})
+      }
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { applyPolyfills, defineCustomElements } from 'stencil-accordion-mekairaw/loader';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
+
+applyPolyfills().then(() => {
+  defineCustomElements(window);
+});
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
